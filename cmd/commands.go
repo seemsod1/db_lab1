af00fbb59639127e1433db2bea20802e66b35894
seemsod <mr.vadym007@gmail.com>
2024-02-13 13:17:30 +0200

    add main functionality

---
+++ cmd/commands.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"github.com/seemsod1/db_lab1/internal/handlers"
+	"github.com/spf13/cobra"
+)
+
+func initRootCmd() *cobra.Command {
+
+	var rootCmd = &cobra.Command{Use: "app"}
+	var insertMCmd = &cobra.Command{
+		Use:   "insert-m <id> <name> <mail> <age>",
+		Short: "Insert master record into file",
+		Long:  ``,
+		Args:  cobra.ExactArgs(4),
+		Run:   handlers.Repo.InsertM,
+	}
+
+	var getMCmd = &cobra.Command{
+		Use:   "get-m <id>",
+		Short: "Get master record from file",
+		Long:  ``,
+		Args:  cobra.ExactArgs(1),
+		Run:   handlers.Repo.GetM,
+	}
+	var insertSCmd = &cobra.Command{
+		Use:   "insert-s <user_id> <rent_id> <price>",
+		Short: "Insert slave record into file",
+		Long:  ``,
+		Args:  cobra.ExactArgs(3),
+		Run:   handlers.Repo.InsertS,
+	}
+	var utilMCmd = &cobra.Command{
+		Use:   "ut-m",
+		Short: "Utility for master file",
+		Long:  ``,
+		Run:   handlers.Repo.UtilM,
+	}
+	var utilSCmd = &cobra.Command{
+		Use:   "ut-s",
+		Short: "Utility for slave file",
+		Long:  ``,
+		Run:   handlers.Repo.UtilS,
+	}
+	var getSCmd = &cobra.Command{
+		Use:   "get-s <user_id> <rent_id>",
+		Short: "Get slave record from file",
+		Long:  ``,
+		Args:  cobra.ExactArgs(2),
+		Run:   handlers.Repo.GetS,
+	}
+	var updateMCmd = &cobra.Command{
+		Use:   "update-m <id> <name> <mail> <age>",
+		Short: "Update master record in file",
+		Long:  ``,
+		Args:  cobra.ExactArgs(4),
+		Run:   handlers.Repo.UpdateM,
+	}
+
+	var updateSCmd = &cobra.Command{
+		Use:   "update-s <user_id> <rent_id> <price>",
+		Short: "Update slave record in file",
+		Long:  ``,
+		Args:  cobra.ExactArgs(3),
+		Run:   handlers.Repo.UpdateS,
+	}
+
+	rootCmd.AddCommand(utilSCmd)
+	rootCmd.AddCommand(utilMCmd)
+	rootCmd.AddCommand(insertSCmd)
+	rootCmd.AddCommand(insertMCmd)
+	rootCmd.AddCommand(getMCmd)
+	rootCmd.AddCommand(getSCmd)
+	rootCmd.AddCommand(updateMCmd)
+	rootCmd.AddCommand(updateSCmd)
+
+	return rootCmd
+
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,152 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/kballard/go-shellquote"
+	"github.com/seemsod1/db_lab1/internal/config"
+	"github.com/seemsod1/db_lab1/internal/handlers"
 	"github.com/seemsod1/db_lab1/internal/helpers"
-	"github.com/seemsod1/db_lab1/internal/models"
-	"log"
 	"os"
+	"strings"
 )
 
-const filename = "user.bin"
+var app config.AppConfig
 
 func main() {
-	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
+
+	file, pos := helpers.OpenMasterFile("user.fl", true)
+	app.MasterFL = file
+	app.MasterPos = pos
+
+	file, pos = helpers.OpenSlaveFile("order.fl", false)
+	app.SlaveFL = file
+	app.SlavePos = pos
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	rootCmd := initRootCmd()
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Interactive mode. Type 'exit' to quit.")
+	for {
+		fmt.Print("$ ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error reading command: %v\n", err)
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+		if input == "exit" {
+			fmt.Println("Exiting...")
+			break
+		}
+
+		args, err := shellquote.Split(input)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error parsing command: %v\n", err)
+			continue
+		}
+
+		rootCmd.SetArgs(args)
+		if err := rootCmd.Execute(); err != nil {
+			fmt.Fprintf(os.Stderr, "error executing command: %v\n", err)
+		}
+	}
+
+}
+
+/*
+Creating linked list of orders
+
+	var writePos int64 = 0
+	var prevPos int64 = -1
+	order := models.Order{
+		UserId: 1,
+		RentId: 1,
+		Price:  100,
+		Next:   -1,
+	}
+	helpers.AddNode(order, file, writePos, prevPos)
+	prevPos = writePos
+	writePos += int64(unsafe.Sizeof(order))
+
+	order = models.Order{
+		UserId: 1,
+		RentId: 2,
+		Price:  200,
+		Next:   -1,
+	}
+	helpers.AddNode(order, file, writePos, prevPos)
+	prevPos = writePos
+	writePos += int64(unsafe.Sizeof(order))
+
+	order = models.Order{
+		UserId: 1,
+		RentId: 3,
+		Price:  300,
+		Next:   -1,
+	}
+	helpers.AddNode(order, file, writePos, prevPos)
+	prevPos = writePos
+	writePos += int64(unsafe.Sizeof(order))
+
+	helpers.PrintNodes(file, 0)
+*/
+
+/*
+Creating list of users
+file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	user := models.User{}
-	helpers.ReadModel(file, &user)
-	fmt.Println(user.Name)
+	var writePos int64 = 0
 
-	helpers.ReadModel(file, &user)
-	fmt.Println(user.Name)
+	user := models.User{ID: 1}
+	copy(user.Name[:], "Ivan")
+	copy(user.Mail[:], "ivan@example.com")
+	user.Age = 20
+	user.FirstOrder = -1
 
-}
+	helpers.WriteModel(file, user, writePos)
+	writePos += int64(unsafe.Sizeof(user))
+
+	user = models.User{ID: 2}
+	copy(user.Name[:], "Vlad")
+	copy(user.Mail[:], "vlad@example.com")
+	user.Age = 18
+	user.FirstOrder = -1
+
+	helpers.WriteModel(file, user, writePos)
+	writePos += int64(unsafe.Sizeof(user))
+
+	user = models.User{ID: 3}
+	copy(user.Name[:], "Vadim")
+	copy(user.Mail[:], "vadim@example.com")
+	user.Age = 19
+	user.FirstOrder = -1
+
+	helpers.WriteModel(file, user, writePos)
+	writePos += int64(unsafe.Sizeof(user))
+
+	user = models.User{ID: 4}
+	copy(user.Name[:], "Dima")
+	copy(user.Mail[:], "dima@example.com")
+	user.Age = 18
+	user.FirstOrder = -1
+
+	helpers.WriteModel(file, user, writePos)
+	writePos += int64(unsafe.Sizeof(user))
+
+	user = models.User{ID: 5}
+	copy(user.Name[:], "Den")
+	copy(user.Mail[:], "den@example.com")
+	user.Age = 17
+	user.FirstOrder = -1
+
+	helpers.WriteModel(file, user, writePos)
+	writePos += int64(unsafe.Sizeof(user))
+*/
